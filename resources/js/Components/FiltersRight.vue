33e5a5e8aaa2a528e0417ca622e270b08ec81361
klemixo <111058299+klemixo@users.noreply.github.com>
2025-02-27 18:55:31 +0100

    Update FiltersRight.vue

--- resources/js/Components/FiltersRight.vue
+++ resources/js/Components/FiltersRight.vue
@@ -89,7 +89,7 @@
             ><span>
               <svg width="12px" height="10px" viewbox="0 0 12 10">
                 <polyline points="1.5 6 4.5 9 10.5 1"></polyline></svg></span
-            ><span>IAAI</span>
+            ><span>IAAIsssssssssss</span>
           </label>
         </div>
         <div class="base-checkbox">
